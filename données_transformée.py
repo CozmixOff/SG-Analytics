45271872cd69f543ea01405b600c24607b28cf52
Aruket <aruket.ipa@gmail.com>
2023-11-14 13:10:06 +0100

    Update données_transformée.py

--- données_transformée.py
+++ données_transformée.py
@@ -1,2 +1,25 @@
-from import_infos_perso import infos_perso
-print(infos_perso(typevalue_vagues))
+import random
+
+def typeur (L):
+    typevalue_L = []
+    for i in range(len(L)):
+        if L[i] not in typevalue_L:
+            typevalue_L.append(L[i])
+    return sorted(typevalue_L)
+
+def transformateur (L):
+    typevalue_L = typeur(L)
+    transfo_L = []
+    for i in L: 
+        x = typevalue_L.index(i)
+        transfo_L.append(x)
+    
+    return transfo_L
+
+liste = list([random.randint(0,100) for i in range(100)])
+
+#print(liste, typeur(liste), transformateur(liste))
+print(liste)
+print(typeur(liste))
+print(transformateur(liste))
+
